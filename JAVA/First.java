355891752e76657e4cd8d8c06424f1d1b1680fc8
Aldin Ariel Pramudya <88958719+aldinpramudya@users.noreply.github.com>
2021-10-16 14:28:38 +0700

    menambahkan nama class sama dengan nama file

--- JAVA/First.java
+++ JAVA/First.java
@@ -1,5 +1,6 @@
 //Setiap perintah di java harus dimasukkan dalam bentuk class
 //perintah awal
+//nama class harus sama dengan file yang akan diinstall
 public class First{
   //main method
   public static void main(String[] args){
