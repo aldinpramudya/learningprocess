d1ea06fce572dfc8517bdb4422780b1bb4f5afe8
Aldin Ariel Pramudya <88958719+aldinpramudya@users.noreply.github.com>
2021-10-22 07:26:07 +0700

    membuat cara print (output)

---
+++ JAVA/print.java
@@ -0,0 +1,11 @@
+public class print{
+  public static void main(String[] args){
+  /*untuk mengeluarkan perintah yang telah ditulis di java pada command prompt,
+  diperlukan kode perintah untuk menampilkan input yang kita telah masukkan sebagai 
+  output*/
+  //dapat ditulis dengan System.out.println("kata kata");
+   System.out.println("Hello World");
+  
+  
+  }
+}
